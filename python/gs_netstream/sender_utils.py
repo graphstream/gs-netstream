12a6c5e35f7397697363314fa75ed0042c2533ab
max <maksim.kalganov@gmail.com>
2022-05-18 11:18:02 +0300

    add typings

--- python/gs_netstream/sender_utils.py
+++ python/gs_netstream/sender_utils.py
@@ -1,19 +1,19 @@
 """Sender encoding utils"""
 import struct
 from itertools import chain
-from typing import List, Callable
+from typing import List, Callable, Any, Optional
 
 from .constants import *
 import numpy as np
 
 
-def encoding_size(value: int):
+def encoding_size(value: int) -> int:
     """Computes the encoding size of a value."""
     dist = (ENCODING_SIZES - value) <= 0
     return 9 if not np.all(dist) else np.argmin(dist) + 1
 
 
-def encode_unsigned(value):
+def encode_unsigned(value: int) -> bytearray:
     """Encodes a Python integer into its varint representation."""
     assert isinstance(value, int) and value >= 0, f"Value argument is not an integer or is negative = {value}"
     size = encoding_size(value)
@@ -34,7 +34,7 @@ TYPES_CONVERTER = {
 }
 
 
-def get_type(value):
+def get_type(value: Any) -> int:
     """Get the data type for a given value."""
     is_array = isinstance(value, list)
     value_type_str = type(value[0] if is_array else value).__name__
@@ -52,38 +52,38 @@ def encode_array(values_array: List, single_value_type, encoding_method: Callabl
     return bytearray(chain(encode_unsigned(len(values_array)), *[encoding_method(elem) for elem in values_array]))
 
 
-def encode_boolean(value):
+def encode_boolean(value: bool) -> bytearray:
     """Encode a boolean type."""
     return bytearray([value & 1])
 
 
-def encode_boolean_array(value: List[bool]):
+def encode_boolean_array(value: List[bool]) -> bytearray:
     """Encode an array of boolean values."""
     return encode_array(value, bool, encode_boolean)
 
 
-def encode_int_array(value):
+def encode_int_array(value: List[int]) -> bytearray:
     """Encode an array of integer values."""
     return encode_array(value, int, encode_unsigned)
 
 
-def encode_double(value):
+def encode_double(value: int) -> bytearray:
     """Encode a double type."""
     return bytearray(struct.pack("!d", value))
 
 
-def encode_double_array(value):
+def encode_double_array(value: List[int]) -> bytearray:
     """Encode an array of double values."""
     return encode_array(value, float, encode_double)
 
 
-def encode_string(string):
+def encode_string(string: str) -> bytearray:
     """Encode a string type."""
     data = bytearray(string, "UTF-8")
     return bytearray(chain(encode_unsigned(len(data)), data))
 
 
-def encode_byte(value):
+def encode_byte(value) -> bytearray:
     """Encode a byte type."""
     return bytearray([value])
 
@@ -93,15 +93,13 @@ TYPE_TO_ENCODER = {
     TYPE_BOOLEAN_ARRAY: encode_boolean_array,
     TYPE_INT: encode_unsigned,
     TYPE_INT_ARRAY: encode_int_array,
-    TYPE_LONG: encode_unsigned,
-    TYPE_LONG_ARRAY: encode_int_array,
     TYPE_DOUBLE: encode_double,
     TYPE_DOUBLE_ARRAY: encode_double_array,
     TYPE_STRING: encode_string
 }
 
 
-def encode_value(value, dtype):
+def encode_value(value: Any, dtype: int) -> Optional[bytearray]:
     """Encode a value according to a given data type."""
     encoder = TYPE_TO_ENCODER[dtype]
     return encoder(value) if encoder is not None else None
