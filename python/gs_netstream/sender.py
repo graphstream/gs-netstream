92be28867d476378a16cfe42d2fde11ef6a9a6a9
max <maksim.kalganov@gmail.com>
2022-05-18 13:11:04 +0300

    refactor netstream proxy graph

--- python/gs_netstream/sender.py
+++ python/gs_netstream/sender.py
@@ -324,7 +324,11 @@ class NetStreamProxyGraph:
     """
 
     def __init__(self, sender, source_id=None):
-        """Constructor can be with one NetStreamSender object and a source id OR with with 4 args: Source ID, Stream ID, Host, and port number"""
+        """Constructor can be with one NetStreamSender object and a source id OR with with 4 args.
+
+        Notes:
+            4 args: Source ID, Stream ID, Host, and port number
+        """
         self.sender = sender
         self.source_id = source_id if source_id else "nss%d" % (1000 * random())
         self.time_id = 0
@@ -339,70 +343,56 @@ class NetStreamProxyGraph:
 
     def remove_node(self, node: str):
         """Remove a node from the graph."""
-        self.sender.node_removed(self.source_id, self.time_id, node)
-        self.time_id += 1
+        self.run_sender_method(self.sender.node_removed, node)
 
     def add_edge(self, edge: str, from_node: str, to_node: str, directed: bool = False):
         """Add an edge to the graph."""
-        self.sender.edge_added(self.source_id, self.time_id, edge, from_node, to_node, directed)
-        self.time_id += 1
+        self.run_sender_method(self.sender.edge_added, edge, from_node, to_node, directed)
 
     def remove_edge(self, edge: str):
         """Remove an edge from the graph."""
-        self.sender.edge_removed(self.source_id, self.time_id, edge)
-        self.time_id += 1
+        self.run_sender_method(self.sender.edge_removed, edge)
 
     def add_attribute(self, attribute: str, value):
         """Add an attribute to the graph."""
-        self.sender.graph_attribute_added(self.source_id, self.time_id, attribute, value)
-        self.time_id += 1
+        self.run_sender_method(self.sender.graph_attribute_added, attribute, value)
 
     def remove_attribute(self, attribute: str):
         """Remove an attribute from the graph."""
-        self.sender.graph_attribute_removed(self.source_id, self.time_id, attribute)
-        self.time_id += 1
+        self.run_sender_method(self.sender.graph_attribute_removed, attribute)
 
     def change_attribute(self, attribute: str, old_value, new_value):
         """Change an attribute of the graph."""
-        self.sender.graph_attribute_changed(self.source_id, self.time_id, attribute, old_value, new_value)
-        self.time_id += 1
+        self.run_sender_method(self.sender.graph_attribute_changed, attribute, old_value, new_value)
 
     def add_node_attribute(self, node: str, attribute: str, value):
         """Add an attribute to a node."""
-        self.sender.node_attribute_added(self.source_id, self.time_id, node, attribute, value)
-        self.time_id += 1
+        self.run_sender_method(self.sender.node_attribute_added, node, attribute, value)
 
     def remove_node_attibute(self, node: str, attribute: str):
         """Remove an attribute from a node."""
-        self.sender.node_attribute_removed(self.source_id, self.time_id, node, attribute)
-        self.time_id += 1
+        self.run_sender_method(self.sender.node_attribute_removed, node, attribute)
 
     def change_node_attribute(self, node: str, attribute: str, old_value, new_value):
         """Change an attribute of a node."""
-        self.sender.node_attribute_changed(self.source_id, self.time_id, node, attribute, old_value, new_value)
-        self.time_id += 1
+        self.run_sender_method(self.sender.node_attribute_changed, node, attribute, old_value, new_value)
 
     def add_edge_attribute(self, edge: str, attribute: str, value):
         """Add an attribute to an edge."""
-        self.sender.edge_attribute_added(self.source_id, self.time_id, edge, attribute, value)
-        self.time_id += 1
+        self.run_sender_method(self.sender.edge_attribute_added, edge, attribute, value)
 
     def remove_edge_attribute(self, edge: str, attribute: str):
         """Remove an attribute from an edge."""
-        self.sender.edge_attribute_removed(self.source_id, self.time_id, edge, attribute)
-        self.time_id += 1
+        self.run_sender_method(self.sender.edge_attribute_removed, edge, attribute)
 
     def change_edge_attribute(self, edge: str, attribute: str, old_value, new_value):
         """Change an attribute of an edge."""
-        self.sender.edge_attribute_changed(self.source_id, self.time_id, edge, attribute, old_value, new_value)
-        self.time_id += 1
+        self.run_sender_method(self.sender.edge_attribute_changed, edge, attribute, old_value, new_value)
 
     def clear_graph(self):
         """Clear the graph."""
-        self.sender.graph_cleared(self.source_id, self.time_id)
-        self.time_id += 1
+        self.run_sender_method(self.sender.graph_cleared)
 
     def step_begins(self, time: int):
         """Begin a step."""
-        self.sender.step_begun(self.source_id, self.time_id, time)
-        self.time_id += 1
+        self.run_sender_method(self.sender.step_begun, time)
